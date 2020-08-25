43302c79cd759026835e641b09c5ceb6ed0df059
simonmungi <simonmungi@gmail.com>
2020-08-25 15:29:08 -0300

    TP 3 primer commit

---
+++ TP3/TP3/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace TP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbDistribuciones = new System.Windows.Forms.ComboBox();
+            this.txtIntervalos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGradosLibertad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.txtVarianza = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbDistribuciones
+            // 
+            this.cmbDistribuciones.FormattingEnabled = true;
+            this.cmbDistribuciones.Location = new System.Drawing.Point(82, 25);
+            this.cmbDistribuciones.Name = "cmbDistribuciones";
+            this.cmbDistribuciones.Size = new System.Drawing.Size(121, 21);
+            this.cmbDistribuciones.TabIndex = 0;
+            // 
+            // txtIntervalos
+            // 
+            this.txtIntervalos.Location = new System.Drawing.Point(283, 137);
+            this.txtIntervalos.Name = "txtIntervalos";
+            this.txtIntervalos.Size = new System.Drawing.Size(100, 20);
+            this.txtIntervalos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Intervalos";
+            // 
+            // txtGradosLibertad
+            // 
+            this.txtGradosLibertad.Location = new System.Drawing.Point(322, 244);
+            this.txtGradosLibertad.Name = "txtGradosLibertad";
+            this.txtGradosLibertad.Size = new System.Drawing.Size(100, 20);
+            this.txtGradosLibertad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Grados de Libertad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(456, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Media";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(459, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Varianza";
+            // 
+            // txtMedia
+            // 
+            this.txtMedia.Location = new System.Drawing.Point(520, 104);
+            this.txtMedia.Name = "txtMedia";
+            this.txtMedia.Size = new System.Drawing.Size(100, 20);
+            this.txtMedia.TabIndex = 7;
+            // 
+            // txtVarianza
+            // 
+            this.txtVarianza.Location = new System.Drawing.Point(520, 137);
+            this.txtVarianza.Name = "txtVarianza";
+            this.txtVarianza.Size = new System.Drawing.Size(100, 20);
+            this.txtVarianza.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(474, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "σ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(474, 225);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "λ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(520, 204);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(520, 231);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtVarianza);
+            this.Controls.Add(this.txtMedia);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtGradosLibertad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIntervalos);
+            this.Controls.Add(this.cmbDistribuciones);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbDistribuciones;
+        private System.Windows.Forms.TextBox txtIntervalos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGradosLibertad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMedia;
+        private System.Windows.Forms.TextBox txtVarianza;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
